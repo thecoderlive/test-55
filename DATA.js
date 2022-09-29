9c7d92bfb86b0efdf7cc8bddd71dbabb75cd1f96
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-09-29 06:55:54 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "45c0b0c8-8dd3-41dc-ba75-5e13957136e5",
+    "home_feed": [
+        {
+            "id": "64ffb853-7c9e-4501-9f62-27c3555e6edb",
+            "artist_name": "information",
+            "home_carousel": [
+                {
+                    "id": "6bb3db08-ae51-42c1-a7f6-c7a028661a34",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/88.png"
+                },
+                {
+                    "id": "47378c96-005b-4cbc-9673-52d78384b9be",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/170.png"
+                },
+                {
+                    "id": "c6d34d91-0e38-45c9-85fe-09b4da992a6d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/359.png"
+                },
+                {
+                    "id": "ad68b4a5-61c6-44af-92f5-49e7590ee538",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/184.png"
+                },
+                {
+                    "id": "6bcf2e24-3f14-47f4-bd2c-abb651e75a51",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/231.png"
+                }
+            ],
+            "art_caption": "although",
+            "art_description": "Woman son treatment. Partner thing wide evidence space. Walk scientist area recently then director."
+        },
+        {
+            "id": "3dcf435b-34a8-4e94-9485-b2d03058faad",
+            "artist_name": "majority",
+            "home_carousel": [
+                {
+                    "id": "83b5c6ee-6ab9-4a96-8eb9-4ed532f63c9e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/207.png"
+                },
+                {
+                    "id": "ab94e7aa-be93-441c-9d10-3ab812a7824b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/298.png"
+                },
+                {
+                    "id": "ce605c99-de5c-456d-b888-f8ade055eab3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/11.png"
+                },
+                {
+                    "id": "82c58b54-a05f-4c37-b909-1a273968e588",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/312.png"
+                },
+                {
+                    "id": "136b62f4-a0ba-4f3c-8dbd-111ad01d6662",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/240.png"
+                }
+            ],
+            "art_caption": "focus",
+            "art_description": "Knowledge second in want economy off task. Road card exist provide. Teacher college war field you prove deal."
+        },
+        {
+            "id": "1d4a3c89-1587-44fe-884c-674d538d0705",
+            "artist_name": "particular",
+            "home_carousel": [
+                {
+                    "id": "c69a68d2-f291-4200-821b-9cb80fc70ef5",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/198.png"
+                },
+                {
+                    "id": "1aa70c8b-0d13-4b57-aaaf-66676e241e05",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/197.png"
+                },
+                {
+                    "id": "0780e4bd-e8d7-4f89-8baa-9d8084ef539c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/187.png"
+                },
+                {
+                    "id": "82d01dfe-1fee-4c46-ba84-f2b4098f00a3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/158.png"
+                },
+                {
+                    "id": "5b93850e-fcb0-47bc-8ad4-f89ef2bebea9",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/307.png"
+                }
+            ],
+            "art_caption": "walk",
+            "art_description": "Second kid war all there poor right. Serve item performance value raise too. Everybody control sound begin present doctor commercial."
+        },
+        {
+            "id": "15da2c01-8aff-4615-a48e-7a7cfe8058e8",
+            "artist_name": "dog",
+            "home_carousel": [
+                {
+                    "id": "15336817-0bea-4847-9aac-3c9fe41c43f4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/158.png"
+                },
+                {
+                    "id": "3a8118aa-92b0-4076-85ef-9b9e5322681e",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/281.png"
+                },
+                {
+                    "id": "12016770-9287-4b53-afc6-a5a1c4fbd73d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/95.png"
+                },
+                {
+                    "id": "a6cd9003-72d8-48de-b48f-64cebd43058d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/363.png"
+                },
+                {
+                    "id": "fbc74afa-828e-46f5-a01a-2765c4452f16",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/10.png"
+                }
+            ],
+            "art_caption": "provide",
+            "art_description": "Create interview through international head recent. Stuff usually over both head once Mrs."
+        },
+        {
+            "id": "ed3a0564-53e0-4c86-92c4-7717ed3f6c11",
+            "artist_name": "sure",
+            "home_carousel": [
+                {
+                    "id": "69411745-6e9d-482e-a49c-269c5b26fe55",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/358.png"
+                },
+                {
+                    "id": "4cb73502-b7c9-45ee-85b2-18358a17787d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/236.png"
+                },
+                {
+                    "id": "69c03b07-ee5e-4ca6-9e0d-693c4464b895",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/298.png"
+                },
+                {
+                    "id": "19fe9acb-7157-4216-862d-a753c27b2eea",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/307.png"
+                },
+                {
+                    "id": "93a88f3b-2faa-47f4-b565-f3e2745768cc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/269.png"
+                }
+            ],
+            "art_caption": "weight",
+            "art_description": "She serious test everything other sister. Sense although full. If state they site bad above right."
+        }
+    ]
+}
